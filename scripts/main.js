c49da2b24a33c5139806e9c0bb9b74b3b6f47f9b
cjaworski27 <collinjaworski97@gmail.com>
2025-02-25 21:40:22 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.log('Document is ready!');
 });
+{
+  "scripts": {
+    "build:azure": "npm install && npm run build"
+  }
+}
